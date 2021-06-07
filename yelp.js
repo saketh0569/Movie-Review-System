5e103d3c4680c21152d240a246e13a8cd2c69731
saketh <mkrishnasaketh2001@gmail.com>
2021-06-07 08:41:14 +0530

    checking

--- yelp.js
+++ yelp.js
@@ -1,45 +1,45 @@
-const express		= require('express'),
-	apk 			= express(),
-	bodyParser		= require('body-parser'),
-	mongoose 		= require('mongoose'),
-	Movie 			= require("./models/movies.js"),
-	seedDB			= require("./seeds.js"),
-	passport		= require("passport"),
-	LocalStrategy	= require('passport-local'),
-	methodOveride	= require('method-override'),
-	User			= require('./models/user.js'),
-	flash 			= require('connect-flash'),
-	logger 			=require('morgan')
-	Comment 		= require("./models/comment.js");
+const express = require('express'),
+	apk = express(),
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose'),
+	Movie = require("./models/movies.js"),
+	seedDB = require("./seeds.js"),
+	passport = require("passport"),
+	LocalStrategy = require('passport-local'),
+	methodOveride = require('method-override'),
+	User = require('./models/user.js'),
+	flash = require('connect-flash'),
+	logger = require('morgan')
+Comment = require("./models/comment.js");
 
 const commentRoutes = require("./routes/comments"),
 	moviesRoutes = require("./routes/movies"),
 	indexRoutes = require("./routes/index");
 
-const DB_URL=process.env.DB_URL || "mongodb+srv://saketh:saketh@cluster0.np0za.mongodb.net/<dbname>?retryWrites=true&w=majority";
-const PORT = process.env.PORT ||80;
+const DB_URL = process.env.DB_URL || "mongodb+srv://saketh:saketh@cluster0.np0za.mongodb.net/movieReviews?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 80;
 const IP = process.env.IP || "127.0.0.1"
 
 apk.set("view engine", "ejs");
-apk.use(bodyParser.urlencoded({extended: true}));
-apk.use(express.static(__dirname+"/public"));
+apk.use(bodyParser.urlencoded({ extended: true }));
+apk.use(express.static(__dirname + "/public"));
 apk.use(methodOveride("_method"));
 apk.use(logger('dev'));
 
-mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 mongoose.set('useFindAndModify', false);
 apk.use(require("express-session")({
-	secret:"I have to complete this by today",
-	resave:false,
-	saveUninitialized:false
+	secret: "I have to complete this by today",
+	resave: false,
+	saveUninitialized: false
 }));
 apk.use(flash());
-apk.use((request,respond,next)=>{
+apk.use((request, respond, next) => {
 	respond.locals.currentUser = request.user;
-	respond.locals.error =request.flash("error");
-	respond.locals.success =request.flash("success");
+	respond.locals.error = request.flash("error");
+	respond.locals.success = request.flash("success");
 	next();
 });
 
@@ -53,14 +53,14 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-apk.use("/",indexRoutes);
-apk.use("/movies",moviesRoutes);
-apk.use("/movies/:id/comments",commentRoutes);
+apk.use("/", indexRoutes);
+apk.use("/movies", moviesRoutes);
+apk.use("/movies/:id/comments", commentRoutes);
 
 
-apk.get("*",(request,respond)=>{
+apk.get("*", (request, respond) => {
 	respond.send("Use a Valid Url");
 });
-apk.listen(PORT,()=>{
-	console.log(process.env.PORT,process.env.IP);
+apk.listen(PORT, () => {
+	console.log(process.env.PORT, process.env.IP);
 });
